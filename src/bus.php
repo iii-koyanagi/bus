dbeef4234034f494a695cfadfd5fe8766d2224ff
iii-koyanagi <koyanagi@iii-store.com>
2015-06-17 17:14:27 +0900

    各人数数え

--- src/bus.php
+++ src/bus.php
@@ -17,6 +17,28 @@ class bus
 
         $passengersArray = explode(",", $passengersWithOutColon);
 
-        var_dump($passengersArray);
+        $adult_number = 0;
+        $child_number = 0;
+        $infant_number = 0;
+        foreach ($passengersArray as $passenger){
+            $age = substr($passenger, 0, 1);
+            $status = substr($passenger, 1, 1);
+
+            if ($age === 'A') {
+                $adult_number += 1;
+            }
+
+            if ($age === 'C') {
+                $child_number += 1;
+            }
+
+            if ($age === 'I') {
+                $infant_number += 1;
+            }
+        }
+
+//        var_dump('A='.$adult_number);
+//        var_dump('C='.$child_number);
+//        var_dump('I='.$infant_number);
     }
 }
